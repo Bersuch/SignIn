1ef430f39e3484c9c3d96a801c7a0ef7adbd8265
Bersuch <bersuchdaniel@yahoo.de>
2020-09-06 13:51:00 +0200

    jQuery functionality

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
+    }
+
+    $("#submit-button").click(function () {
+        var errorMessage = "";
+
+        if (isValidEmailAddress($("#email").val()) == false) {
+            errorMessage += "<p>Deine Emailadresse ist inkorrekt</p>";
+        }
+        
+        if($("#tel").val().toString().length < 5) {
+            errorMessage += "<p>Deine Nummer ist zu kurz</p>";
+        }
+        
+        if($("#passw").val() != $("#passw2").val()) {
+            errorMessage += "<p>Deine Passwörter stimmen nicht überein</p>";
+        }
+
+        if(errorMessage != "") {
+            $("#errorMessage").html(errorMessage); 
+            $("#successMessage").hide();
+            $("#errorMessage").show();
+        } else {
+            $("#successMessage").show();
+            $("#errorMessage").hide();
+        }
+        
+        errorMessage = "";
+    });
+
+});
